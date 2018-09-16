be28946bac94f2d185ff3dd619f1508ed9b28fdf
Bulat Ion <nelu.bulat@hotmail.com>
2018-09-16 17:56:07 +0300

    Added var 3 formula

--- grafica/lab1/ion/var3.cpp
+++ grafica/lab1/ion/var3.cpp
@@ -1,11 +1,43 @@
 #include <iostream> 
+#include <cmath>
+
+const float PI = 3.1415926;
 
 using namespace std;
 
-int main() {
-    return 0;
+float my_pow(float x, int n) {
+    if (n < 0) {
+        x = 1 / x;
+        n = -n; 
+    }
+    if (n == 0) {
+        return 1;
+    }
+    float y = 1;
+    while (n > 1) {
+        if (n % 2 == 0) {
+            x = x * x;
+            n = n / 2;
+        } else {
+            y = x * y;
+            x = x * x;
+            n = (n - 1) / 2;
+        }
+    } 
+    return x * y;
+}
+//arcctg
+float formula(int x, int epsilon) {
+    float summ_result = 0;
+    for (int i = 0; i < epsilon; i++) {
+        summ_result += (my_pow(-1, i + 1) * ( my_pow(x, 2 * i + 1) / (2 * i + 1) ) );
+    }
+    return PI / 2 + summ_result;
 }
 
-double arctg(int x, double epsilon) {
-    return 0;
+int main() {
+    //formulea mea pentr arc cotagenta
+    cout << formula(1, 5000) << endl;
+    // din bibloteca math
+    cout << PI / 2 - atan(1) << endl;
 }
