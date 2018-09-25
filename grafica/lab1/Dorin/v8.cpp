34c351f9191b986c430034bc3242c1003406c349
icedant3 <33224293+icedant3@users.noreply.github.com>
2018-09-25 15:47:16 +0300

    Update v8.cpp

--- grafica/lab1/Dorin/v8.cpp
+++ grafica/lab1/Dorin/v8.cpp
@@ -1,43 +1,33 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
-double power(double x, int y)
-{
-    double z;
-    if (y == 0)
-    return 1;
-    z = power (x, y / 2);
-    if ((y % 2) == 0) {
-        return z * z;
-    } else {
-        if (y > 0)
-            return x * z * z;
-        else
-            return (z* z) / x;
-    }
-}
-
-int factorial(int k){
-    int fact = k;
-    while(--k){
-        fact *= k;
-    }
-    return fact;
-}
-
-double formula(double x, double eps){
+double ln_1_min_x(double x, double eps){
+	if(x < -1 || x >= 1){
+        std::cerr<<"wrong value\n";
+        return 0;
+	}
 	double sum = x;
-	if (eps > 1)
-		for (int k=2; k<eps; k++){
-			sum += power(x, k)/factorial(k);
-		}
+	double t = x;
+	double k = 2;
+
+	do{
+        t *=x/k;
+
+        sum += t;
+        std::cout<<sum<<"\n";
+	}while(fabs(t)>eps);
 	return sum;
 }
-	
+
 
 int main(){
+	/*
+	 * -1 <= x < 1
+	 */
 	double x, eps;
 	std::cout<<"x="; std::cin>>x;
 	std::cout<<"eps="; std::cin>>eps;
-	std::cout<<"result "<<std::setprecision(9)<<formula(x, eps);
+	std::cout<<"result "<<std::setprecision(9)<<ln_1_min_x(x, eps);
+    std::cout<<"log 1 -x " <<std::cout<<log(1 - x)<<"\n";
 }
