2f04a4e275cc634cf2cc96b2a93c35d774096504
icedant3 <33224293+icedant3@users.noreply.github.com>
2018-09-25 16:12:12 +0300

    Update v8.cpp

--- grafica/lab1/Dorin/v8.cpp
+++ grafica/lab1/Dorin/v8.cpp
@@ -29,9 +29,5 @@ int main(){
 	std::cout<<"x="; std::cin>>x;
 	std::cout<<"eps="; std::cin>>eps;
 	std::cout<<"result "<<std::setprecision(9)<<formula(x, eps);
-    /*
-     * poti sa stergi si linia 36 sau in "pune formula ta din cmath"
-     * si in loc de log(1-x) la fel formula ta din varianta ta
-     */
-    std::cout<<"log 1 -x " <<std::cout<<log(1 - x)<<"\n";
+   
 }
