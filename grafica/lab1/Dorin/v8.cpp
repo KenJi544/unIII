edf19f073f88093beb0101b3d2c1a9b5b91fd7a2
Grigori Draci <grigori@smartsoftdev.eu>
2018-09-25 15:30:24 +0300

    indicatii v8.cpp

--- grafica/lab1/Dorin/v8.cpp
+++ grafica/lab1/Dorin/v8.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 #include <cmath>
 
-double ln_1_min_x(double x, double eps){
+double formula(double x, double eps){
 	if(x < -1 || x >= 1){
         std::cerr<<"wrong value\n";
         return 0;
@@ -28,6 +28,10 @@ int main(){
 	double x, eps;
 	std::cout<<"x="; std::cin>>x;
 	std::cout<<"eps="; std::cin>>eps;
-	std::cout<<"result "<<std::setprecision(9)<<ln_1_min_x(x, eps);
+	std::cout<<"result "<<std::setprecision(9)<<formula(x, eps);
+    /*
+     * poti sa stergi si linia 36 sau in "pune formula ta din cmath"
+     * si in loc de log(1-x) la fel formula ta din varianta ta
+     */
     std::cout<<"log 1 -x " <<std::cout<<log(1 - x)<<"\n";
 }
